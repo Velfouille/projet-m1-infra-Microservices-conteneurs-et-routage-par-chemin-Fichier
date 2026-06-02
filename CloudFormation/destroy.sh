b5aea69c2847376e35fb95e703ec7b1774b6d29e
Mathias Bredeville <mathias.bredeville@ynov.com>
2026-06-02 11:53:51 +0200

    update destroy.sh

--- CloudFormation/destroy.sh
+++ CloudFormation/destroy.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+set -e
 
-echo "🧹 Début de la destruction de l'infrastructure StreamFlex..."
+REGION="us-east-1"
+MASTER_STACK_NAME="StreamFlex-Master"
+TEMPLATE_BUCKET="s3-streamflex-templates-mbn" 
+FRONTEND_BUCKET="s3-projet-m1-infra-cloud-mbn"
 
-# ÉTAPE 0 : Vider le bucket S3 (Sinon CloudFormation plantera)
-echo "🗑️  0/3 : Vidage du bucket S3 front-end..."
-aws s3 rm s3://s3-projet-m1-infra-cloud-mbn --recursive || true
+echo "🧹 Début de la destruction de l'infrastructure globale StreamFlex..."
 
-echo "1/3 : Suppression des conteneurs ECS..."
-aws cloudformation delete-stack --stack-name StreamFlex-ECS --region us-east-1
-aws cloudformation wait stack-delete-complete --stack-name StreamFlex-ECS --region us-east-1
+# ÉTAPE 0 : Vider les deux buckets S3 (Front-end et Templates)
+echo "🗑️  0/2 : Vidage des buckets S3 pour autoriser la suppression..."
+aws s3 rm s3://$FRONTEND_BUCKET --recursive || true
+aws s3 rm s3://$TEMPLATE_BUCKET --recursive || true
 
-echo "2/3 : Suppression du Load Balancer..."
-aws cloudformation delete-stack --stack-name StreamFlex-ALB --region us-east-1
-aws cloudformation wait stack-delete-complete --stack-name StreamFlex-ALB --region us-east-1
+# ÉTAPE 1 : Suppression de la Master Stack (qui détruit automatiquement les 3 autres)
+echo "🔥 1/2 : Suppression de la Master Stack (ECS, ALB, Réseau)..."
+aws cloudformation delete-stack --stack-name $MASTER_STACK_NAME --region $REGION
 
-echo "3/3 : Suppression du Réseau..."
-aws cloudformation delete-stack --stack-name StreamFlex-Network --region us-east-1
-aws cloudformation wait stack-delete-complete --stack-name StreamFlex-Network --region us-east-1
+# On dit au terminal de patienter jusqu'à la fin de la destruction
+aws cloudformation wait stack-delete-complete --stack-name $MASTER_STACK_NAME --region $REGION
 
-echo "✅ Destruction terminée ! Plus rien ne t'est facturé."
+# ÉTAPE 2 : Suppression du bucket de templates (créé manuellement au déploiement)
+echo "📦 2/2 : Suppression du bucket technique S3..."
+aws s3 rb s3://$TEMPLATE_BUCKET --force || true
+
+echo "------------------------------------------------------"
+echo "✅ Destruction terminée avec succès !"
+echo "Plus aucune ressource n'est active, ta facturation est à zéro."
+echo "------------------------------------------------------"
